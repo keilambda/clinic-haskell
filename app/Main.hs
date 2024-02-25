7eeb5ecb4983358f9e8bfb5a80568952a3940e5b
thelissimus <thelissimus@tuta.io>
2024-02-25 14:58:08 +0500

    refactor: move entrypoint to `Main`

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,38 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE ImplicitParams #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main (main) where
 
-import Clinic qualified
+import Clinic.API (app)
+import Clinic.Database (migrate')
+
+import Control.Monad.Logger (NoLoggingT (runNoLoggingT))
+
+import Data.ByteString (ByteString)
+import Data.Kind (Type)
+
+import Database.Persist.Postgresql (createPostgresqlPool)
+
+import Network.Wai.Handler.Warp (run)
+
+import Options.Generic
+
+type Opts :: Type -> Type
+data Opts w = MkOpts
+  { port :: !(w ::: Int <?> "Port to listen on" <!> "8080" <#> "p")
+  , dbUrl :: !(w ::: ByteString <?> "Database connection string" <#> "u")
+  , dbPoolSize :: !(w ::: Int <?> "Database pool size" <!> "10" <#> "s")
+  }
+  deriving stock (Generic)
+
+deriving anyclass instance ParseRecord (Opts Wrapped)
+deriving stock instance Show (Opts Unwrapped)
 
 main :: IO ()
-main = Clinic.main
+main = do
+  (opts :: Opts Unwrapped) <- unwrapRecord "Clinic API"
+  pool <- runNoLoggingT $ createPostgresqlPool opts.dbUrl opts.dbPoolSize
+  let ?pool = pool
+  migrate'
+  run opts.port app
--- src/Clinic.hs
+++ src/Clinic.hs
@@ -1,39 +1,7 @@
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE ImplicitParams #-}
-{-# LANGUAGE OverloadedStrings #-}
+module Clinic (
+  module Clinic.API,
+  module Clinic.Database,
+) where
 
-module Clinic (main) where
-
-import Clinic.API (app)
-import Clinic.Database (migrate')
-
-import Control.Monad.Logger (NoLoggingT (runNoLoggingT))
-
-import Data.Kind (Type)
-
-import Database.Persist.Postgresql (createPostgresqlPool)
-
-import Network.Wai.Handler.Warp (run)
-
-import Options.Generic
-
-import Data.ByteString (ByteString)
-
-type Opts :: Type -> Type
-data Opts w = MkOpts
-  { port :: !(w ::: Int <?> "Port to listen on" <!> "8080" <#> "p")
-  , dbUrl :: !(w ::: ByteString <?> "Database connection string" <#> "u")
-  , dbPoolSize :: !(w ::: Int <?> "Database pool size" <!> "10" <#> "s")
-  }
-  deriving stock (Generic)
-
-deriving anyclass instance ParseRecord (Opts Wrapped)
-deriving stock instance Show (Opts Unwrapped)
-
-main :: IO ()
-main = do
-  (opts :: Opts Unwrapped) <- unwrapRecord "Clinic API"
-  pool <- runNoLoggingT $ createPostgresqlPool opts.dbUrl opts.dbPoolSize
-  let ?pool = pool
-  migrate'
-  run opts.port app
+import Clinic.API
+import Clinic.Database
